c436389063babaee57271fbe270c8d13905b54c3
diegocampanale <campanale.die@gmail.com>
2025-03-26 00:22:56 +0100

    landing page, hero done, features done

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from "./components/Navbar";
 import HeroSection from './components/HeroSection';
+import Features from './components/Features';
 
 export const App = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Features />
     </>
   )
 }
--- landing-page/src/components/HeroSection.jsx
+++ landing-page/src/components/HeroSection.jsx
@@ -9,8 +9,8 @@ const HeroSection = () => {
             <div className="flex flex-wrap lg:block lg:pb-[90px] justify-center">
                 {/* Testi */}
                 <div className="flex flex-wrap w-auto lg:w-[620px] text-center lg:text-left">
-                    <h1 className="text-[38px] lg:text-[67px] font-bold tracking-[0.02] lg:tracking-tight leading-[1.3] text-[#2D2D2D]">Create Engaging Landing Pages</h1>
-                    <p className="fontHeroHeader text-[#2D2D2D] lg:text-[21px] font-[400] leading-[1.5] font-mulish mt-[20px]">
+                    <h1 className="text-[38px] lg:text-[67px] font-bold -tracking-[0.02] lg:tracking-tight leading-[1.3] text-[#2D2D2D]">Create Engaging Landing Pages</h1>
+                    <p className="text-[#2D2D2D] lg:text-[21px] font-[400] leading-[1.5] font-mulish mt-[20px]">
                         Build beautiful landing pages in record time with <a href="#" className="text-blue-500">Anima</a>’s Landing Page UI kit for Figma. No code required!</p>
                 </div>
                 {/* CTAs */}
@@ -23,7 +23,7 @@ const HeroSection = () => {
                 </div>
             </div>
             {/* Mockup*/}
-            <div className="flex w-full lg:w-auto justify-center lg:justify-end mt-[60px] lg-mt-auto">
+            <div className="flex w-full lg:w-auto justify-center lg:justify-end mt-[60px] lg:mt-auto">
                 <img className="lg:max-w-[450px]" src={mockup} alt="logo" />
             </div>
         </div>
