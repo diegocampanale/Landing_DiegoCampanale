251f8465b6d7b0c9e1f48b131c8111ce253767a7
diegocampanale <campanale.die@gmail.com>
2025-03-28 17:05:57 +0100

    final fix

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -4,8 +4,8 @@ const TestimonialCard = ({ name, role, image, text, rating = 5 }) => {
 
   return (
     <div className="flex gap-[30px] p-[40px] bg-[#FFFFFF] rounded-[20px] border border-[#E5F4F2] max-w-[570px] drop-shadow-[0_35px_30px_rgba(51,102,255,0.05)]">
-        <div className="flex flex-col lg:flex-row gap-[30px] items-center">
-            <div className="flex flex-col gap-[16px] min-w-[180px] items-center">
+        <div className="flex flex-col sm:flex-row gap-[30px] items-center">
+            <div className="flex flex-col gap-[16px] md:min-w-[180px] items-center">
                 <img src={image} alt="photo1" className='w-[120px]'/>
                 <div className="flex flex-col gap-[6px] text-center">
                     <h2 className='font-[#111827] text-[21px] font-semibold leading-[1.3]'>{name}</h2>
